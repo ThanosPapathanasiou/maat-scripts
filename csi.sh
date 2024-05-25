9cf9a93e9b5c9c5c0542a205cae549eca43b0080
Thanos Papathanasiou <thanospapathanasiou@hotmail.com>
2024-05-25 18:08:12 +0100

    add age analysis chart

--- csi.sh
+++ csi.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 
-# You can call this script from inside a git directory and it will generate a d3 chart
+# To setup: create the alias bellow
+# alias csi='bash LOCATION_OF_THIS_REPO/csi.sh'
+# After the alias has been set you can call this script from inside any git repo and it will:
+# 1. Generate d3 charts with all the metrics it can
+# 2. Start a webserver for you to check the charts 
 
 if git rev-parse &>/dev/null; then
-  #alias csi='bash ~/projects/tools/csi.sh'
+
   maat_location=$(dirname "$0")/code-maat-1.0.4-standalone.jar
   scripts_location=$(dirname "$0")
   output=crime-scene-analysis
 
   mkdir $output
 
-  # create data
-  git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > ./$output/git_log.txt
-  java -jar $maat_location -l ./$output/git_log.txt -c git2 -a revisions > ./$output/revisions.csv
-  cloc --unix --vcs git --by-file --csv --quiet --timeout 10 --report-file=./$output/complexity.csv
+  # create data                                                                                                         # List of:
+  cloc --unix --vcs git --by-file --csv --quiet --timeout 10 --report-file=./$output/complexity.csv                     # - files
+  git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > ./$output/git_log.txt            # - commits
+  java -jar $maat_location -l ./$output/git_log.txt -c git2 -a revisions  > ./$output/analysis_revisions.csv            # - files, number of revisions
+  java -jar $maat_location -l ./$output/git_log.txt -c git2 -a age        > ./$output/analysis_age.csv                  # - files, age 
 
   cd $output
 
   # transform data
-  python3 $scripts_location/merge/merge_comp_freqs.py revisions.csv complexity.csv
-  python3 $scripts_location/transform/csv_as_enclosure_json.py --structure complexity.csv --weights revisions.csv > hotspots.json
+  python3 $scripts_location/merge/merge_comp_freqs.py analysis_revisions.csv complexity.csv
+  python3 $scripts_location/transform/csv_as_enclosure_json.py          --structure complexity.csv --weights analysis_revisions.csv > hotspots.json
+  python3 $scripts_location/transform/code_age_csv_as_enclosure_json.py --structure complexity.csv --weights       analysis_age.csv > age.json
 
   # prepare visualisation website
-  cp $scripts_location/transform/crime-scene-hotspots.html index.html
   cp -r $scripts_location/transform/d3 d3
+  cp $scripts_location/transform/crime-scene-hotspots.html hotspots.html
+  cp $scripts_location/transform/crime-scene-age.html      age.html
+  
 
   # run webserver
   echo ""
