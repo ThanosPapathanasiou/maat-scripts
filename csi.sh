04156e6f81ebf186d4e20e5e07e991377e6087ee
Thanos Papathanasiou <thanospapathanasiou@hotmail.com>
2024-05-26 02:13:38 +0100

    add communication analysis support

--- csi.sh
+++ csi.sh
@@ -22,27 +22,33 @@ if git rev-parse &>/dev/null; then
   git log --all --numstat --date=short --pretty=format:'--%h--%ad--%aN' --no-renames > ./$output/git_log.txt # - commits
 
   # analyze data
-  declare -a analysis_types=("age" "revisions")
-  declare -a all_analysis_types=("abs-churn" "age" "author-churn" "authors" "communication" "coupling" "entity-churn" "entity-effort" "entity-ownership" "fragmentation" "identity" "main-dev" "main-dev-by-revs" "messages" "refactoring-main-dev" "revisions" "soc" "summary")
-  for i in "${analysis_types[@]}"
-  do
-    echo "Performing '$i' analysis"
-    java -jar $maat_location -l ./$output/git_log.txt -c git2 -a $i > ./$output/analysis_$i.csv
+  # key value pair: (analysis, d3 visualization)
+  declare -A analysis_types
+  analysis_types["age"]="enclosure"
+  analysis_types["revisions"]="enclosure"
+  analysis_types["communication"]="edge_bundling"
+  # TODO: add support for all the available analysis options
+  # declare -a all_analysis_types=("abs-churn" "age" "author-churn" "authors" "communication" "coupling" "entity-churn" "entity-effort" "entity-ownership" "fragmentation" "identity" "main-dev" "main-dev-by-revs" "messages" "refactoring-main-dev" "revisions" "soc" "summary")
+
+  for key in ${!analysis_types[@]}; do
+    # echo ${key} ${analysis_types[${key}]}
+    echo "Performing '${key}' analysis"
+    java -jar $maat_location -l ./$output/git_log.txt -c git2 -a ${key} > ./$output/analysis_${key}.csv
   done
 
   cd $output
 
   # transform data
-  python3 $scripts_location/merge/merge_comp_freqs.py analysis_revisions.csv complexity.csv                                         > /dev/null 2>&1 # don't print anything
-  python3 $scripts_location/transform/csv_as_enclosure_json.py          --structure complexity.csv --weights analysis_revisions.csv > revisions.json
-  python3 $scripts_location/transform/code_age_csv_as_enclosure_json.py --structure complexity.csv --weights       analysis_age.csv > age.json
+  python3 $scripts_location/merge/merge_comp_freqs.py analysis_revisions.csv complexity.csv                                             > /dev/null 2>&1 # don't print anything
+  python3 $scripts_location/transform/code_age_csv_as_enclosure_json.py     --structure complexity.csv --weights       analysis_age.csv > age.json
+  python3 $scripts_location/transform/csv_as_enclosure_json.py              --structure complexity.csv --weights analysis_revisions.csv > revisions.json
+  python3 $scripts_location/transform/communication_csv_as_edge_bundling.py --communication analysis_communication.csv                  > communication.json
 
   # prepare visualisation website
   cp -r $scripts_location/transform/d3 d3
-  for i in "${analysis_types[@]}"
-  do
-    cp $scripts_location/transform/crime-scene.html analysis_$i.html
-    sed -i "s/INPUT/$i.json/g" analysis_$i.html
+  for key in ${!analysis_types[@]}; do
+    cp $scripts_location/transform/${analysis_types[${key}]}.html analysis_${key}.html
+    sed -i "s/INPUT/${key}.json/g" analysis_${key}.html
   done
 
   printf "Analysis finished: "
